7899274990ee402c930cbe51e42c7a261461e565
richard-mcghee <richardjmcghee@yahoo.com>
2020-08-26 01:26:45 -0400

    Made some major changes. Finished setting up the private route and adjusted routing a bit in app. Built out the instructor workout list and created unique routing for instructor workout list and workout since they will need to update and create them, built the bones of the post request in the workout form, built the delete request in the instructor workout component, will need to check the specifics of the workout object from backend to further setup the instructor workout component to give it relevant data to update and pre-fill the form. Added an instructor link to the home page which links to the instructor workout list.

--- fitness/src/components/instructors/InstructorWorkout.js
+++ fitness/src/components/instructors/InstructorWorkout.js
@@ -1,16 +1,102 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams, useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+import { axiosAuth } from '../../utilities/apiUtilities'
+import { updateTrue } from '../../actions/usersActions'
+import { connect } from 'react-redux'
 
-const InstructorWorkout = () => {
+const StyleSimply = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+const InstructorWorkout = (props) => {
     const { id } = useParams()
     const { push } = useHistory()
+    const initState = {
+        name: "",
+        instructor_name: "",
+        type: "",
+        intensity: "",
+        location: "",
+        date: "11/10/1775",
+        max_size: "",
+        duration: 30.00,
+        signedUp: false
+    }
+    const [classToUpdate, setClassToUpdate] = useState(initState)
 
     useEffect(() => {
-        // Will grab the individual workout from paramater id and render it with prefilled values, will re-render based on id per dependency array 
+        axiosAuth().get(`https://anywhere-fitness-team2.herokuapp.com/api/auth/users/classes/${id}`)
+        .then(res => {
+            console.log(res)
+            setClassToUpdate(res.data.data)
+        })
+        .catch(err => console.dir(err))
     },[id])
+
+    const handleChanges = evt => {
+        setClassToUpdate({
+            ...classToUpdate,
+            [evt.target.name]: evt.target.value
+        })
+    }
+    const handleSubmit = evt => {
+        evt.preventDefault()
+    }
+
+    const handleUpdate = evt => {
+        evt.preventDefault()
+        // This will handle the update and hence the put request
+        push(`/workout-Instructor/${id}`)
+        props.updateTrue() 
+    }
+    const handleDelete = evt => {
+        evt.preventDefault()
+        axiosAuth().delete(`https://anywhere-fitness-team2.herokuapp.com/api/auth/instructor/classes/${id}`)
+        .then(res => console.log(res))
+        .catch(err => console.dir(err))
+        push("/list-Instructor")
+        props.updateTrue()
+    }
+    // This form will probably need to be massively tweaked based on how exactly the backend structures the data to determine relevant fields to display and how they should be structured for updating
     return ( 
-        <h1>Placeholder</h1>
+        <StyleSimply>
+            <form onSubmit={handleSubmit}>
+                <input type="text"
+                name=""
+                value={classToUpdate}
+                onChange={handleChanges} />
+                <input type="text"
+                name=""
+                value={classToUpdate}
+                onChange={handleChanges} />
+                <input type="text"
+                name=""
+                value={classToUpdate}
+                onChange={handleChanges} />
+                <input type="text"
+                name=""
+                value={classToUpdate}
+                onChange={handleChanges} />
+                <input type="text"
+                name=""
+                value={classToUpdate}
+                onChange={handleChanges} />
+                <input type="text"
+                name=""
+                value={classToUpdate}
+                onChange={handleChanges} />
+                <input type="text"
+                name=""
+                value={classToUpdate}
+                onChange={handleChanges} />
+
+                <button className="update-workout" onClick={handleUpdate}>Update Class</button>
+                <button className="delete-workout" onClick={handleDelete}>!Delete Class!</button>
+            </form>
+        </StyleSimply>
      );
 }
  
-export default InstructorWorkout;
+export default connect(null, { updateTrue } )(InstructorWorkout);
